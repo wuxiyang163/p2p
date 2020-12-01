aae62d6fb1fdeb431fa23247a31604743a959ee1
wuxiyang163 <wuxiyang163126@163.com>
2020-12-01 12:25:02 +0800

    增加变量

--- src/com/bjpowernode/Test.java
+++ src/com/bjpowernode/Test.java
@@ -3,7 +3,8 @@ package com.bjpowernode;
 public class Test {
 
     public static void main(String[] args) {
-
+        int i =2;
+        int j =3;
         String text = "老铁，你好了没？";
         System.out.println("淦");
     }
